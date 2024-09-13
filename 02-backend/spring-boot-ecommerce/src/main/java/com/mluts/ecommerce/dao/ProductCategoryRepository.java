0a7095ea5adc8710765dffa9901e8883c7efa009
Lutsyk Mykola <lutsyk.mikolaj@gmail.com>
2024-09-13 17:01:58 +0200

    initial commit, adding entities and configuring properties

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/mluts/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.mluts.ecommerce.dao;
+
+import com.mluts.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
