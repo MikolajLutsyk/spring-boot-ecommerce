f9ac914d09c74fbb0b29c3e0351faff9ad8b7107
Lutsyk Mykola <lutsyk.mikolaj@gmail.com>
2024-09-15 19:26:49 +0200

    1. angular project created 2.angular component for product-list created 3.TS class for Product created 4. angular service for calling rest apis created 5.angular component altered to subscribe to data from service 6.html page now displays data in a table 7.CrossOrigin support added to backend app

--- 02-backend/spring-boot-ecommerce/src/main/java/com/mluts/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/mluts/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.mluts.ecommerce.dao;
 
 import com.mluts.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
